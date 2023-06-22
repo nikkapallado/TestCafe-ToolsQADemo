dd4c35dae5172f5cd003af1264058119d2d52f79
nikkapallado <ntpallado@gmail.com>
2023-06-22 09:09:20 +0800

    feat: add web table fixture and tests

---
+++ test/WebTableTest.js
@@ -0,0 +1,56 @@
+import { Selector, ClientFunction} from "testcafe";
+
+//Selector object syntax
+const getPageURL = ClientFunction(() => {
+    return window.location.href;
+});
+
+//Parameter function syntax
+const getWebElementById = Selector(id =>{
+    return document.getElementById(id);
+});
+
+const getWebElementByClass = Selector(className => {
+    return document.getElementsByClassName(className);
+});
+
+//options dependency syntax use this if you do not need to pass different values everytime you call it
+const persistentId = "item-3";
+const element = Selector(() =>{
+    return document.getElementById(persistentId);
+}, {
+    dependencies:{persistentId}
+})
+
+fixture("Web Table Fixture")
+    .page("https://demoqa.com/elements")
+    .skipJsErrors();
+
+test.skip("Navigate to Web Table Test - Using Selector object", async t =>{
+    const webTableLink = Selector("#item-3");
+    const webTableHeaderText = Selector(".main-header").innerText;
+    await t
+        .maximizeWindow()
+        .click(webTableLink)
+        .expect(getPageURL()).contains("webtables")
+        .expect(webTableHeaderText).eql("Web Tables");
+});
+
+test.skip("Navigate to Web Table Test - Using Document method", async t =>{
+    const webTableLink = getWebElementById("item-3");
+    const webTableHeaderText = getWebElementByClass("main-header").innerText;
+    await t
+        .maximizeWindow()
+        .click(webTableLink)
+        .expect(getPageURL()).contains("/webtables")
+        .expect(webTableHeaderText).eql("Web Tables");
+});
+
+test("Navigate to Web Table Test - Using Options dependency", async t =>{
+    const webTableHeaderText = getWebElementByClass("main-header").innerText;
+    await t
+        .maximizeWindow()
+        .click(element)
+        .expect(getPageURL()).contains("/webtables")
+        .expect(webTableHeaderText).eql("Web Tables");
+});
