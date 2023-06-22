83acd906f88d4e60321ec2f741158d58c356feb4
nikkapallado <ntpallado@gmail.com>
2023-06-22 10:33:31 +0800

    chore: add additional test

--- test/WebTableTest.js
+++ test/WebTableTest.js
@@ -1,6 +1,5 @@
 import { Selector, ClientFunction} from "testcafe";
 
-//Selector object syntax
 const getPageURL = ClientFunction(() => {
     return window.location.href;
 });
@@ -20,7 +19,7 @@ const element = Selector(() =>{
     return document.getElementById(persistentId);
 }, {
     dependencies:{persistentId}
-})
+});
 
 fixture("Web Table Fixture")
     .page("https://demoqa.com/elements")
@@ -46,11 +45,49 @@ test.skip("Navigate to Web Table Test - Using Document method", async t =>{
         .expect(webTableHeaderText).eql("Web Tables");
 });
 
-test("Navigate to Web Table Test - Using Options dependency", async t =>{
+test.skip("Navigate to Web Table Test - Using Options dependency", async t =>{
     const webTableHeaderText = getWebElementByClass("main-header").innerText;
     await t
         .maximizeWindow()
         .click(element)
         .expect(getPageURL()).contains("/webtables")
         .expect(webTableHeaderText).eql("Web Tables");
+});
+
+test.skip("Navigate to Web Table Test - Using Selector Child", async t =>{
+    const webTableLink = Selector("ul.menu-list").child(3);
+    const webTableHeaderText = getWebElementByClass("main-header").innerText;
+    await t
+        .maximizeWindow()
+        .click(webTableLink)
+        .expect(getPageURL()).contains("/webtables")
+        .expect(webTableHeaderText).eql("Web Tables");
+});
+
+test.skip("Navigate to Web Table Test - Using Selector withAttr", async t =>{
+    const webTableLink = Selector("#item-3");
+    const webTableHeaderText = Selector(".main-header").innerText;
+    const addButton = Selector("button").withAttribute("id", "addNewRecordButton");
+    const registrationFormModalTitle = Selector("#registration-form-modal");
+    await t
+        .maximizeWindow()
+        .click(webTableLink)
+        .expect(getPageURL()).contains("webtables")
+        .expect(webTableHeaderText).eql("Web Tables")
+        .click(addButton)
+        .expect(registrationFormModalTitle.innerText).eql("Registration Form");
+});
+
+test("Navigate to Web Table Test - Using Selector withExactText", async t =>{
+    const webTableLink = Selector("#item-3");
+    const webTableHeaderText = Selector(".main-header").innerText;
+    const addButton = Selector("button").withExactText("Add");
+    const registrationFormModalTitle = Selector("#registration-form-modal");
+    await t
+        .maximizeWindow()
+        .click(webTableLink)
+        .expect(getPageURL()).contains("webtables")
+        .expect(webTableHeaderText).eql("Web Tables")
+        .click(addButton)
+        .expect(registrationFormModalTitle.innerText).eql("Registration Form");
 });
