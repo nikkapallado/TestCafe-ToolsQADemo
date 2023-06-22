e04f001ad3a3e2369efe8581a7a82d19d08cc26f
nikkapallado <ntpallado@gmail.com>
2023-06-22 15:52:56 +0800

    feat: add DOM node state method tests

--- test/CheckboxTest.js
+++ test/CheckboxTest.js
@@ -8,7 +8,7 @@ fixture("Web Table Fixture")
     .page("https://demoqa.com/elements")
     .skipJsErrors();
 
-test("Click on all first level child checkboxes Test - Using Selector count and nth", async t =>{
+test.skip("Click on all first level child checkboxes Test - Using Selector count and nth", async t =>{
     const checkboxLink = Selector("#item-1");
     const checkboxHeaderText = Selector(".main-header").innerText;
     const parentToggleButton = Selector("button.rct-collapse.rct-collapse-btn");
@@ -34,5 +34,63 @@ test("Click on all first level child checkboxes Test - Using Selector count and 
     const selectedListText = Selector("#result").innerText;
     await t
         .expect(selectedListText).eql("You have selected :\nhome\ndesktop\nnotes\ncommands\ndocuments\nworkspace\nreact\nangular\nveu\noffice\npublic\nprivate\nclassified\ngeneral\ndownloads\nwordFile\nexcelFile")
+
+});
+
+test.skip("Verify if checkbox is checked", async t =>{
+    const checkboxLink = Selector("#item-1");
+    const checkboxHeaderText = Selector(".main-header").innerText;
+    const parentCheckbox = Selector("#tree-node-home");
+    const parentfolder = Selector("label[for='tree-node-home']");
+
+    await t
+        .maximizeWindow()
+        .click(checkboxLink)
+        .expect(getPageURL()).contains("checkbox")
+        .expect(checkboxHeaderText).eql("Check Box")
+        .expect(parentCheckbox.checked).notOk()
+        .click(parentfolder)
+        .expect(parentCheckbox.checked).ok();
+});
+
+test.skip("Verify if element has class test", async t =>{
+    const checkboxLink = Selector("#item-1");
+    const checkboxHeaderText = Selector(".main-header").innerText;
+    const parentDropdown = Selector("button[title='Toggle']").nth(0);
+    const parentfolder = Selector("label[for='tree-node-home']");
+
+    await t
+        .maximizeWindow()
+        .click(checkboxLink)
+        .expect(getPageURL()).contains("checkbox")
+        .expect(checkboxHeaderText).eql("Check Box")
+        .expect(parentDropdown.classNames).eql(["rct-collapse", "rct-collapse-btn"]);
+});
+
+test.skip("Verify the element text test", async t =>{
+    const checkboxLink = Selector("#item-1");
+    const checkboxHeaderText = Selector(".main-header").innerText;
+    const parentTitle = Selector(".rct-title").nth(0);
+
+    await t
+        .maximizeWindow()
+        .click(checkboxLink)
+        .expect(getPageURL()).contains("checkbox")
+        .expect(checkboxHeaderText).eql("Check Box")
+        .expect(parentTitle.innerText).eql("Home");
+});
+
+test("Verify the element's style property", async t =>{
+    const checkboxLink = Selector("#item-1");
+    const checkboxHeaderText = Selector(".main-header").innerText;
+    const parentFolderIcon = Selector(".rct-icon.rct-icon-parent-close").nth(0);
+    const parentfolder = Selector("label[for='tree-node-home']");
+
+    await t
+        .maximizeWindow()
+        .click(checkboxLink)
+        .expect(getPageURL()).contains("checkbox")
+        .expect(checkboxHeaderText).eql("Check Box")
+        .expect(parentFolderIcon.getStyleProperty("color")).eql("rgb(51, 51, 204)")
 
 });
